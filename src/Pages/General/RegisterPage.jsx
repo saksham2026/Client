227ef87c411aa80f3298322725ffb1081e1c2b00
Saksham <sakshamiitroorkee3@gmail.com>
2023-12-30 01:48:29 +0530

    made changes in register page

--- src/Pages/General/RegisterPage.jsx
+++ src/Pages/General/RegisterPage.jsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 import Loader from "../../Components/Loader";
 import { useNavigate } from "react-router-dom";
 function RegisterPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   function validateEmail() {
     let email = document.getElementById('email').value;
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,10 +26,10 @@ function RegisterPage() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    if(validateEmail()){
+    if (validateEmail()) {
       if (
         document.querySelector("#password").value !==
-        document.querySelector("#confirmPassword").value 
+        document.querySelector("#confirmPassword").value
       ) {
         alert("Password and Confirm Password must be same");
       } else {
@@ -48,117 +48,132 @@ function RegisterPage() {
           .then((res) => {
             setLoading(false);
             navigate("/login");
-            
+
           })
           .catch((error) => {
-            if(error.response.status==410) alert("Username already taken.")
-            if(error.response.status==409) alert("Email is already taken.")
-            if(error.response.status==411) alert("Phone Number already taken")
-            setLoading(false)});
+            if (error.response.status == 410) alert("Username already taken.")
+            if (error.response.status == 409) alert("Email is already taken.")
+            if (error.response.status == 411) alert("Phone Number already taken")
+            setLoading(false)
+          });
       }
     }
-    
+
   }
 
   return (
     <>
       {loading && <Loader />}
 
-      <div className="bg-neutral-800 text-yellow-300 h-screen w-screen overflow-hidden flex flex-col items-center gap-8">
+      <div className="bg-neutral-950 text-yellow-300 h-screen w-screen overflow-hidden flex flex-col items-center gap-8 py-5">
         <Navbar />
         <form
           id="register-container"
-          className="px-3  flex flex-col gap-2 items-center w-full sm:w-3/5 lg:w-1/5 overflow-y-scroll"
+          className="px-3 py-3 flex flex-col gap-2 items-center w-full sm:w-[350px] md:w-3/5 overflow-y-scroll border bg-neutral-950 border-gray-600"
           onSubmit={handleSubmit}
         >
           <h1 className="font-bold text-2xl text-green-400">Register</h1>
-          <Input
-            label="First Name"
-            name="firstname"
-            type="text"
-            id="firstname"
-            required={true}
-            inputClass="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg"
-            divClass="flex flex-col w-full"
-          />
-          <Input
-            label="Last Name"
-            name="lastname"
-            type="text"
-            id="lastname"
-            required={true}
-            inputClass="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg"
-            divClass="flex flex-col w-full"
-          />
+          <div className=" sm:flex flex-col md:grid md:grid-cols-2 md:gap-10">
 
-          <Input
-            label="Username"
-            name="username"
-            type="text"
-            id="username"
-            required={true}
-            inputClass="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg"
-            divClass="flex flex-col w-full"
-          />
-          <Input
-            label="Email"
-            name="email"
-            type="text"
-            id="email"
-            required={true}
-            inputClass="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg"
-            divClass="flex flex-col w-full"
-          />
-          <Input
-            label="Phone"
-            name="mobile"
-            type="tel"
-            id="mobile"
-            placeholder="+91 XXXXXXXXXX"
-            pattern="+91 [0-9]{10}"
-            required={true}
-            inputClass="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg"
-            divClass="flex flex-col w-full"
-          />
-          <Select
-            label="Role"
-            name="role"
-            id="role"
-            className="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg text-white"
-            options={["Producer", "Freelancer"]}
-            divClass="flex flex-col w-full"
-            optionsClassname='bg-black'
-          />
-          <Input
-            label="Password"
-            type="password"
-            name="password"
-            id="password"
-            required={true}
-            inputClass="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg"
-            divClass="flex flex-col w-full"
-          />
-          <Input
-            label="Confirm"
-            type="password"
-            name="confirmpassword"
-            id="confirmPassword"
-            required={true}
-            inputClass="bg-transparent border-green-800 border-2 outline-none text-xl px-2 py-1 sm:w-full rounded-lg"
-            divClass="flex flex-col w-full"
-          />
-          <Input
-            label="Upload Profile Image"
-            type="file"
-            name="avatar"
-            divClass="flex flex-col w-full"
-          />
+            <Input
+              label="First Name"
+              name="firstname"
+              type="text"
+              id="firstname"
+              required={true}
+              inputClass="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full"
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+            />
+            <Input
+              label="Last Name"
+              name="lastname"
+              type="text"
+              id="lastname"
+              required={true}
+              inputClass="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full"
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+            />
+
+            <Input
+              label="Username"
+              name="username"
+              type="text"
+              id="username"
+              required={true}
+              inputClass="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full"
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+            />
+            <Input
+              label="Email"
+              name="email"
+              type="text"
+              id="email"
+              required={true}
+              inputClass="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full"
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+            />
+            <Input
+              label="Phone"
+              name="mobile"
+              type="tel"
+              id="mobile"
+              placeholder="+91 XXXXXXXXXX"
+              pattern="+91 [0-9]{10}"
+              required={true}
+              inputClass="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full"
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+            />
+            <Select
+              label="Role"
+              name="role"
+              id="role"
+              className="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full text-white"
+              options={["Producer", "Freelancer"]}
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+              optionsClassname='bg-black'
+            />
+            <Input
+              label="Password"
+              type="password"
+              name="password"
+              id="password"
+              required={true}
+              inputClass="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full"
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+            />
+            <Input
+              label="Confirm"
+              type="password"
+              name="confirmpassword"
+              id="confirmPassword"
+              required={true}
+              inputClass="bg-transparent border-green-800 border-2 outline-none text-2xl px-2 py-2 sm:w-full"
+              divClass="flex flex-col w-full"
+              labelClass="font-bold text-2xl"
+            />
+          </div>
+          <div id="profile-image">
+            <Input
+              id="profile"
+              label="Upload Profile Image"
+              type="file"
+              name="avatar"
+              divClass="flex flex-col w-full items-center"
+            />
+          </div>
           <Buttun
             text="Register"
-            className="bg-green-600 text-white font-bold px-2 py-2 w-44 mt-3 rounded-md"
+            className="bg-green-600 text-white font-bold px-3 py-3 mt-3 w-[200px]"
             type="submit"
           />
-          <p>
+          <p className="font-bold text-xl">
             Already a user?{" "}
             <Link to="/login" className="font-bold ">
               Login
