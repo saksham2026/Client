92888b90ed2e65e0c0ea2d49267ed4d2f1b6f9c2
saksham <sakshamiitroorkee3@gmail.com>
2024-01-04 17:01:18 +0530

    made color changes in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <nav className='w-full h-8 lg:h-16 flex items-center pl-1 sticky text-green-700 font-bold border-b-green-400 sm:text-2xl'>
+      <nav className='entry-nav w-full h-8 lg:h-16 flex items-center px-3 py-5 sticky font-bold border-b-green-400 sm:text-2xl'>
         RetroCraftHub
       </nav>
     </>
